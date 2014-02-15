4561bff5853a196d8883bc253bc5a9f16d6c8064
rogelio.sevilla1 <rogelio.sevilla1@gmail.com>
2014-02-15 14:20:47 -0600

    add guard and spork

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+
+  def full_title(page_title)
+    base_title = "Ruby on Rails Tutorial Sample App"
+    page_title = page_title.empty? ? "" : " | #{page_title}"
+    "#{base_title}#{page_title}"
+  end
+
 end
